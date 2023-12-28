519f9054b87c1d3a4bb2181ecd6cfbaead7946d5
jayna-tanjum <jaynajarin26@gmail.com>
2023-12-28 20:35:52 +0600

    factorial

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class factorial {
+public static void main(String[]args){
+    Scanner sc= new Scanner(System.in);
+    int num1= sc.nextInt();
+    System.out.println("Enter the number: "+num1);
+    int multi=1;
+    for(int i=1; i<num1;i++)
+    {
+         multi*=i;
+    }
+    System.out.println(multi);
+}
+}
