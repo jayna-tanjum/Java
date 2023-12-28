04d07184c9dd865d620c71f103cf9aa994ad14b5
jayna-tanjum <jaynajarin26@gmail.com>
2023-12-28 11:26:09 +0600

    Ascii

---
+++ ASCII.java
@@ -0,0 +1,7 @@
+public class ASCII {
+     public static void main(String[] args) {
+       char ch='A';
+        int Ascii = (int) ch;
+         System.out.println("The ASCII value of " + ch + " is: " + Ascii);
+    }
+}
