e76a443715742e5ab9c5062e1124c02355768c74
jayna-tanjum <jaynajarin26@gmail.com>
2023-12-28 18:41:21 +0600

    reverse

---
+++ reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+       String sentence= sc.nextLine();
+       String reverseString=" ";
+       char ch;
+       for(int i=0;i<sentence.length();i++)
+       {
+        ch= sentence.charAt(i);
+        reverseString=ch+reverseString;
+       }
+System.out.println(reverseString);
+    }
+}
