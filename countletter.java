265ea245d5c1ca5cad22b291c4b8f31f1fc20c60
jayna-tanjum <jaynajarin26@gmail.com>
2023-12-31 21:46:27 +0600

    calender

--- countletter.java
+++ countletter.java
@@ -7,7 +7,7 @@ public class countletter{
         System.out.println(str);    
         for(int i = 0; i < str.length(); i++) 
         {    
-            if(str.charAt(i) != ' ') 
+            if(str.charAt(i) != ' ')
             {
                 count++;    
             }
